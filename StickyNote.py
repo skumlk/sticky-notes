f05408ceb8b869427d7e3b692c11106d1a0228b2
sameera kumarasingha <sameerakumarasingha@gmail.com>
2020-04-30 20:41:36 +0530

    #10:skumlk Add shortcut keys to bold, italic, underline and strickethrough

--- StickyNote.py
+++ StickyNote.py
@@ -5,72 +5,7 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 from Titlebar import TitleBar
 import Const
-
-class StickyNoteEditor(QtWidgets.QTextEdit):
-
-    color_list = ["purple", "green", "pink", "yellow"]
-
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
-        self.parent = parent
-
-    def isSelected(self):
-        cursor = self.textCursor()
-        return cursor.selectionEnd() - cursor.selectionStart()
-
-    def contextMenuEvent(self, event):
-
-        contextMenu = QMenu(self)
-        isSelected = self.isSelected()
-
-        copyAction = contextMenu.addAction("Copy")
-        copyAction.triggered.connect(self.copy_action)
-
-        cutAction = contextMenu.addAction("Cut")
-        cutAction.triggered.connect(self.cut_action)
-
-        pasteAction = contextMenu.addAction("Paste")
-        pasteAction.triggered.connect(self.paste_action)
-
-        deleteAction = contextMenu.addAction("Delete")
-        deleteAction.triggered.connect(self.delete_action)
-
-        selectAllAction = contextMenu.addAction("Select All")
-        selectAllAction.triggered.connect(self.select_all_action)
-
-        if(not isSelected):
-            copyAction.setEnabled(False)
-            cutAction.setEnabled(False)
-            deleteAction.setEnabled(False)
-
-        if not self.canPaste():
-            pasteAction.setEnabled(False)
-       
-        contextMenu.addSeparator()
-
-        for color in self.color_list:
-            contextMenu.addAction(QtGui.QIcon('img/{0}.png'.format(color)), color.title())
-
-        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
-        if action:
-            color =  action.text().lower()
-            if color in self.color_list:
-                self.parent.setColor(color)
-
-    def copy_action(self):
-        self.copy()
-
-    def paste_action(self):
-        self.paste()
-
-    def delete_action(self):
-        pass
-
-    def select_all_action(self):
-        self.selectAll()
-
-    def cut_action(self):
-        self.cut()
+from StickyNoteEditor import StickyNoteEditor
 
 class StickyNote(QtWidgets.QFrame):
 
@@ -118,7 +53,7 @@ class StickyNote(QtWidgets.QFrame):
         self.move(x, y)
 
     def signalTextChanged(self):
-        text = self.textEditor.toPlainText()
+        text = self.textEditor.toHtml()
         self.noteManager.updateNoteText(self._id, text)
 
     def positionChanged(self, x,y):
@@ -151,7 +86,7 @@ class StickyNote(QtWidgets.QFrame):
                 Background:  {0};
                 color:white;
                 font:13px ;
-                font-weight:bold;
+                font-weight:normal;
                 }}
             """.format(backgroundColor)
         self.setStyleSheet(css)
---
+++ StickyNoteEditor.py
@@ -0,0 +1,105 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QFrame, QMenu, QMessageBox
+
+class StickyNoteEditor(QtWidgets.QTextEdit):
+
+    color_list = ["purple", "green", "pink", "yellow"]
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.parent = parent
+
+        boldAction = QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+B"), self)
+        boldAction.activated.connect(self.boldAction)
+
+        italicAction = QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+I"), self)
+        italicAction.activated.connect(self.italicAction)
+
+        strikeThroughAction = QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+S"), self)
+        strikeThroughAction.activated.connect(self.strikeThroughAction)
+
+        unerlineAction = QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+U"), self)
+        unerlineAction.activated.connect(self.underlineAction)
+
+    def isSelected(self):
+        cursor = self.textCursor()
+        return cursor.selectionEnd() - cursor.selectionStart()
+
+    def contextMenuEvent(self, event):
+
+        contextMenu = QMenu(self)
+        isSelected = self.isSelected()
+
+        copyAction = contextMenu.addAction("Copy")
+        copyAction.triggered.connect(self.copy_action)
+
+        cutAction = contextMenu.addAction("Cut")
+        cutAction.triggered.connect(self.cut_action)
+
+        pasteAction = contextMenu.addAction("Paste")
+        pasteAction.triggered.connect(self.paste_action)
+
+        deleteAction = contextMenu.addAction("Delete")
+        deleteAction.triggered.connect(self.delete_action)
+
+        selectAllAction = contextMenu.addAction("Select All")
+        selectAllAction.triggered.connect(self.select_all_action)
+
+        if(not isSelected):
+            copyAction.setEnabled(False)
+            cutAction.setEnabled(False)
+            deleteAction.setEnabled(False)
+
+        if not self.canPaste():
+            pasteAction.setEnabled(False)
+       
+        contextMenu.addSeparator()
+
+        for color in self.color_list:
+            contextMenu.addAction(QtGui.QIcon('img/{0}.png'.format(color)), color.title())
+
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+        if action:
+            color =  action.text().lower()
+            if color in self.color_list:
+                self.parent.setColor(color)
+
+    def copy_action(self):
+        self.copy()
+
+    def paste_action(self):
+        self.paste()
+
+    def delete_action(self):
+        pass
+
+    def select_all_action(self):
+        self.selectAll()
+
+    def cut_action(self):
+        self.cut()
+
+    def boldAction(self):
+        currentFormat = self.currentCharFormat()
+        if currentFormat.fontWeight == 75:
+            newWeight = 50
+        else:
+            newWeight = 75
+
+        currentFormat.setFontWeight(newWeight)
+        self.setCurrentCharFormat(currentFormat)
+
+    def italicAction(self):
+        currentFormat = self.currentCharFormat()
+        currentFormat.setFontItalic(not currentFormat.fontItalic())
+        self.setCurrentCharFormat(currentFormat)
+
+    def underlineAction(self):
+        currentFormat = self.currentCharFormat()
+        currentFormat.setFontUnderline(not currentFormat.fontUnderline())
+        self.setCurrentCharFormat(currentFormat)
+
+    def strikeThroughAction(self):
+        currentFormat = self.currentCharFormat()
+        currentFormat.setFontStrikeOut(not currentFormat.fontStrikeOut())
+        self.setCurrentCharFormat(currentFormat)
